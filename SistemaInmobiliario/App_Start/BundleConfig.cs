7287c806100752fdcff1e6d19ae8aff0a50446e3
Miguel Gonzalez <mig_24@hotmail.com>
2021-10-01 22:35:04 -0500

    Inicializacion del proyecto de inmobiliario

---
+++ SistemaInmobiliario/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.UI;
+
+namespace SistemaInmobiliario
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                            "~/Scripts/modernizr-*"));
+
+			bundles.Add(new ScriptBundle("~/bundles/init").Include(
+						"~/Scripts/jquery-3.4.1.min.js",
+						"~/Scripts/sweetalert2.min.js",
+						"~/Scripts/popper.min.js",
+						"~/Scripts/bootstrap.min.js",
+						"~/Scripts/jquery.mCustomScrollbar.concat.min.js",
+						"~/Scripts/bootstrap-material-design.min.js",
+						"~/Scripts/main.js"));
+		}
+    }
+}
